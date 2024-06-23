96eb898f7551d6acbe7dcb8437ae2c08f60b1e14
naoverthinking <navl029519@gmail.com>
2024-06-23 23:42:18 +0700

    "new commit"

---
+++ JobFlow/settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") } // add this line
+        jcenter()
+    }
+}
+
+rootProject.name = "JobFlow"
+include(":app")
+ 
